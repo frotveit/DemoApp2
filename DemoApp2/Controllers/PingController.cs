65e991511fb5c09d01f612b6e6065f07497d4753
frotveit <frotveit@outlook.com>
2023-01-02 14:50:23 +0100

    Ping

---
+++ DemoApp2/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}
